eed46eaf573495babc2ccbf92b0de08424bbbf80
Jorge Ramirez <joramirez96@gmail.com>
2020-03-30 23:13:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,11 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   dwarves.each_with_index do |name, index|
    index += 1
    puts "#{index}. #{name}"
  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   planeteer_calls.collect do |call|
     call.capitalize << "!"
   end
